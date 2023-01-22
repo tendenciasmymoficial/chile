5495fbeb9ac2baef89dcb663dcb00c151a470ed1
tendenciasmymoficial <110960309+tendenciasmymoficial@users.noreply.github.com>
2023-01-22 00:06:30 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,6 @@ const scrollUp = () =>{
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -54,33 +50,6 @@ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dar
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
 // We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-}
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true
-})
 
-sr.reveal(`.home__data, .products__container, .footer__container, .footer__info`)
-sr.reveal(`.home__images`, {delay: 600, origin: 'bottom'})
-sr.reveal(`.new__card, .brand__img`, {interval: 100})
-sr.reveal(`.collection__explore:nth-child(1)`, {origin: 'right'})
-sr.reveal(`.collection__explore:nth-child(2)`, {origin: 'left'})
